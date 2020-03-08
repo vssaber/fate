50dde74140a87f42c141060546638a9630d1a50f
vssaber <1391526849@qq.com>
2020-03-08 23:12:45 +0800

    test push

--- hello.py
+++ hello.py
@@ -16,3 +16,10 @@ if __name__ == "__main__":
     h2 = Hello(6)
 
     print('hh')
+    print('hh')
+    print('hh')
+    print('hh')
+    print('hh')
+    print('hh')
+    print('hh')
+    print('hh')
